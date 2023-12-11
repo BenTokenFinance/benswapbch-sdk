2f07ebfa27c774410020bd2c320c2de5545c6e29
Ben Token Finance <admin@bentoken.finance>
2023-12-11 19:54:01 +0800

    Update currency.ts

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'BCH', 'Bitcoin Cash')
+  public static readonly ETHER: Currency = new Currency(18, 'SBCH', 'Smart Bitcoin Cash')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
